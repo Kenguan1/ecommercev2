9c16d3aa7822340e38f4c36cad10e55b7772fe10
Camilo Kenguan <49210338+Kenguan1@users.noreply.github.com>
2024-06-09 20:55:03 -0500

    cambio eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'], rules: {'prettier/prettier': 0},
- /* plugins: ['simple-import-sort'], rules: { 'simple-import-sort/imports': error}*/
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
 }
